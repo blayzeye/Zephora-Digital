2dc2aeb1445bc0b5db00088782e80ffd63465f85
Op Bhai <opbhaiontop@gmail.com>
2025-12-26 10:55:46 +0000

    Rebrand the agency into Zephora Digital

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="border-t border-border/40 py-6">
       <div className="container mx-auto px-4 text-center text-sm text-foreground/60">
-        <p>&copy; {new Date().getFullYear()} ConversionFlow. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Zephora Digital. All rights reserved.</p>
       </div>
     </footer>
   );
